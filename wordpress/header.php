d6d73150099f311d466ff6f1e717250017a51147
Arjun Kalburgi <askalburgi@gmail.com>
2016-10-04 01:39:33 -0600

    wordpress-ized now in one folder

---
+++ wordpress/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<!--
+	Astral by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>ECE Week 2017</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="style.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Wrapper-->
+			<div id="wrapper">
+
+				<!-- Nav -->
+					<nav id="nav">
+						<a href="#home" class="icon fa-calendar-o active"><span>Home</span></a>
+						<a href="#talks" class="icon fa-comment"><span>Talks</span></a>
+						<a href="#mixer" class="icon fa-users"><span>Mixer</span></a>
+						<a href="#hack" class="icon fa-code"><span>Hackathon</span></a>
+						<a href="#contact" class="icon fa-envelope"><span>Contact</span></a>
+						<!-- <a href="#" class="icon fa-twitter"><span>Twitter</span></a> -->
+					</nav>
+
---
+++ wordpress/index.php
@@ -0,0 +1,210 @@
+<?php get_header(); ?>
+
+<!-- Main -->
+	<div id="main">
+
+		<!-- Me -->
+			<article id="home" class="panel no-bg">
+				<header>
+					<h1>ECE Week 2017</h1>
+					<p>Celebrating ECE Technology and Education</p>
+				</header>
+				<a class="jumplink pic">
+					<!-- <span class="arrow icon fa-chevron-right"><span>See my work</span></span> -->
+					<img src="images/me.jpg" alt="" />
+				</a>
+			</article>
+
+		<!-- talks -->
+			<article id="talks" class="panel">
+				<header>
+					<h2>Tech Talks</h2>
+					When&Where
+				</header>
+				<p>
+					Tech Talks are short, engaging presentations about interesting technology. A successful Talk will inspire the audience to pursue further knowledge about this technology. Our Tech Talks are limited to 10 minutes, Inkovate2015 has a series of flash talks of varying length on Youtube that demonstrate this idea.
+				</p>
+				<span class="embed-youtube" style="text-align:center; display: block;"><iframe class="youtube-player" type="text/html" width="560" height="315" src="http://www.youtube.com/embed/videoseries?list=PLRp9jPf602CCu8xiaUwp-fZqAnMVAjIZ-&amp;hl=en_US" allowfullscreen="true" style="border:0;"></iframe></span>
+				<p>
+					<h3>Our Talks</h3>
+				</p>
+				<section>
+					<div class="row">
+						<div class="4u 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic01.jpg" alt=""></a>
+						</div>
+						<div class="4u 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic02.jpg" alt=""></a>
+						</div>
+						<div class="4u$ 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic03.jpg" alt=""></a>
+						</div>
+						<div class="4u 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic04.jpg" alt=""></a>
+						</div>
+						<div class="4u 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic05.jpg" alt=""></a>
+						</div>
+						<div class="4u$ 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic06.jpg" alt=""></a>
+						</div>
+						<!-- <div class="4u 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic07.jpg" alt=""></a>
+						</div>
+						<div class="4u 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic08.jpg" alt=""></a>
+						</div>
+						<div class="4u$ 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic09.jpg" alt=""></a>
+						</div>
+						<div class="4u 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic10.jpg" alt=""></a>
+						</div>
+						<div class="4u 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic11.jpg" alt=""></a>
+						</div>
+						<div class="4u$ 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic12.jpg" alt=""></a>
+						</div> -->
+					</div>
+				</section>
+			</article>
+
+		<!-- Mixer -->
+			<article id="mixer" class="panel">
+				<header>
+					<h2>Mixer</h2>
+					When&Where
+				</header>
+				<p>
+					Our mixer takes place on the Friday night of ECE Week. There will be food, beverages and a little friendly competition. As you enter the mixer you will be split into teams and will have the chance to compete for prizes throughout the night with plenty of mixing in between.
+				</p>
+				<section>
+					<div class="row">
+						<div class="4u 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic01.jpg" alt=""></a>
+						</div>
+						<div class="4u 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic02.jpg" alt=""></a>
+						</div>
+						<div class="4u$ 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic03.jpg" alt=""></a>
+						</div>
+						<div class="4u 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic04.jpg" alt=""></a>
+						</div>
+						<div class="4u 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic05.jpg" alt=""></a>
+						</div>
+						<div class="4u$ 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic06.jpg" alt=""></a>
+						</div>
+						<!-- <div class="4u 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic07.jpg" alt=""></a>
+						</div>
+						<div class="4u 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic08.jpg" alt=""></a>
+						</div>
+						<div class="4u$ 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic09.jpg" alt=""></a>
+						</div>
+						<div class="4u 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic10.jpg" alt=""></a>
+						</div>
+						<div class="4u 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic11.jpg" alt=""></a>
+						</div>
+						<div class="4u$ 12u$(mobile)">
+							<a href="#" class="image fit"><img src="images/pic12.jpg" alt=""></a>
+						</div> -->
+					</div>
+				</section>
+			</article>
+
+		<!-- Hack -->
+		<article id="hack" class="panel">
+			<header>
+				<h2>HackED 2017</h2>
+				When&Where
+			</header>
+			<p>
+				A hackathon is a competition with intense collaboration, great ideas and an amazing atmosphere. The hackathon gives you the chance to improve your skills and build your dreams. HackED is a hackathon to build solutions to your everyday problems. You can build anything you want as long as it’s original and you start from scratch! HackED is a hackathon for you, make the most of it!
+			</p>
+			<p>
+				Learn more about Hacked 2017 on the website.
+			</p>
+			<section>
+				<div class="row">
+					<div class="4u 12u$(mobile)">
+						<a href="#" class="image fit"><img src="images/pic01.jpg" alt=""></a>
+					</div>
+					<div class="4u 12u$(mobile)">
+						<a href="#" class="image fit"><img src="images/pic02.jpg" alt=""></a>
+					</div>
+					<div class="4u$ 12u$(mobile)">
+						<a href="#" class="image fit"><img src="images/pic03.jpg" alt=""></a>
+					</div>
+					<div class="4u 12u$(mobile)">
+						<a href="#" class="image fit"><img src="images/pic04.jpg" alt=""></a>
+					</div>
+					<div class="4u 12u$(mobile)">
+						<a href="#" class="image fit"><img src="images/pic05.jpg" alt=""></a>
+					</div>
+					<div class="4u$ 12u$(mobile)">
+						<a href="#" class="image fit"><img src="images/pic06.jpg" alt=""></a>
+					</div>
+					<!-- <div class="4u 12u$(mobile)">
+						<a href="#" class="image fit"><img src="images/pic07.jpg" alt=""></a>
+					</div>
+					<div class="4u 12u$(mobile)">
+						<a href="#" class="image fit"><img src="images/pic08.jpg" alt=""></a>
+					</div>
+					<div class="4u$ 12u$(mobile)">
+						<a href="#" class="image fit"><img src="images/pic09.jpg" alt=""></a>
+					</div>
+					<div class="4u 12u$(mobile)">
+						<a href="#" class="image fit"><img src="images/pic10.jpg" alt=""></a>
+					</div>
+					<div class="4u 12u$(mobile)">
+						<a href="#" class="image fit"><img src="images/pic11.jpg" alt=""></a>
+					</div>
+					<div class="4u$ 12u$(mobile)">
+						<a href="#" class="image fit"><img src="images/pic12.jpg" alt=""></a>
+					</div> -->
+				</div>
+			</section>
+		</article>
+
+		<!-- Contact -->
+			<article id="contact" class="panel">
+				<header>
+					<h2>Contact</h2>
+				</header>
+				<p>
+					For involvement, participation and sponsorship queries, please email us here!
+				</p>
+				<form action="#" method="post">
+					<div>
+						<div class="row">
+							<div class="6u 12u$(mobile)">
+								<input type="text" name="name" placeholder="Name" />
+							</div>
+							<div class="6u$ 12u$(mobile)">
+								<input type="text" name="email" placeholder="Email" />
+							</div>
+							<div class="12u$">
+								<input type="text" name="subject" placeholder="Subject" />
+							</div>
+							<div class="12u$">
+								<textarea name="message" placeholder="Message" rows="8"></textarea>
+							</div>
+							<div class="12u$">
+								<input type="submit" value="Send Message" />
+							</div>
+						</div>
+					</div>
+				</form>
+			</article>
+	</div>
+
+<?php get_footer(); ?>
