d0ed2dea9f07898a22d4a5187f815da8826db92c
Arjun Kalburgi <askalburgi@gmail.com>
2017-01-13 19:36:41 -0700

    updated wp

--- wordpress/index.php
+++ wordpress/index.php
@@ -51,131 +51,161 @@
 			<article id="workshops" class="panel">
 				<header>
 					<h2>Workshops</h2>
-					When&Where
-  					(tuesday and wednesday) - tbd
+					5:00 PM on Tuesday & Wednesday, January 24-25 @ <a href="https://maps.google.com/maps?f=q&hl=en&q=ETLC">ETLC, University of Alberta</a>
 				</header>
 				<p>
 					Workshops emphasize a hands-on approach to learning. Build something cool or learn how to use a new piece of technology in an informative and engaging one hour session! Whether you’re a novice or experienced tinkerer, our workshops will expand your knowledge about what work in the ECE industry looks like.
 				</p>
-		        <p>
-		    	    If you are interested in hosting a workshop, contact eceweek@compeclub.com for details.
-		        </p>
+				<p>
+				If you are interested in hosting a workshop, contact eceweek@compeclub.com for details.
+				</p>
 
 				<section>
 					<h3>Our Workshops</h3>
-					<p style="margin:0px">To be determined - contact for more</p>
+
 					<div class="row" style="margin-top: 0px">
+						<a href="" class="4u 12u$(mobile)">
+							<div class="CE1 workshop">
+								<div class="text">
+									Web API's: Gluing the Modern Web Together
+									<div>Ben Zittlau - Jobber</div>
+									<div>5pm Tuesday, ATCO</div>
+								</div>
+							</div>
+						</a>
 						<div class="4u 12u$(mobile)">
-							<a href="#" class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/static/images/Workshop01.jpg" alt=""></a>
-						</div>
-						<div class="4u 12u$(mobile)">
-							<a href="#" class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/static/images/Workshop02.jpg" alt=""></a>
+							<div class="EE1 workshop">
+								<div class="text">
+									TBA
+								</div>
+							</div>
 						</div>
 						<div class="4u$ 12u$(mobile)">
-							<a href="#" class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/static/images/Workshop03.jpg" alt=""></a>
+							<div class="EP1 workshop">
+								<div class="text">
+									TBA
+								</div>
+							</div>
+						</div>
+					</div>
+					<br>
+					<div class="row" style="margin-top: 0px">
+						<div class="4u 12u$(mobile)">
+							<div class="CE2 workshop">
+								<div class="text">
+									TBA
+									<div>Dark Horse Analytics</div>
+									<div>5pm Wednesday, location TBA</div>
+								</div>
+							</div>
+						</div>
+						<div class="4u 12u$(mobile)">
+							<div class="EE2 workshop">
+								<div class="text">
+									TBA
+								</div>
+							</div>
+						</div>
+						<div class="4u$ 12u$(mobile)">
+							<div class="EP2 workshop">
+								<div class="text">
+									TBA
+								</div>
+							</div>
 						</div>
 					</div>
 				</section>
 			</article>
 
-		<!-- democamp/keynote 
+		<!-- lunchalytics -->
+			<article id="lunchalytics" class="panel">
+				<header>
+					<h2>Lunchalytics</h2>
+					 11:45 AM on Wednesday, January 25 @ <a href="https://maps.google.com/maps?f=q&hl=en&q=301%2C+10359+-+104+Street%2C+Edmonton%2C+AB%2C+ca">StartUpEdmonton</a>
+				</header>
+				<p>Lunchalytics is a monthly event by Dark Horse Analytics and StartUp Edmonton. <br/><br/> Get a free lunch and learn about open source and open data analytics by Mark Diner (Chief Advisor, Open Government at Government of Alberta) and Taj Darra (Open Source Analytics Data Scientist at IBM)
+				</p>
+				<p>Learn more and RSVP <a href="https://www.meetup.com/startupedmonton/events/234082671/?isFromReg=true">here</a></p>
+			</article>
+
+		<!-- democamp/keynote -->
 			<article id="democamp" class="panel">
 				<header>
 					<h2>Democamp</h2>
-					When&Where
-			        (Thursday, January 26th at tbd) - tbd
+					5:00 PM on Tuesday & Wednesday, January 24-25 @ <a href="http://maps.google.com/?q=11335%20Saskatchewan%20Drive%20Northwest%20Edmonton,%20AB,%20T6G%202M9%20Canada">CCIS 1-140, University of Alberta</a>
 				</header>
 				<p>
+					(Hosted by Startup Edmonton)<br>
 					DemoCamp brings together developers, creatives, entrepreneurs and investors to share what they’ve been working on and to find others in the community interested in similar topics. For presenters, it’s a great way to get feedback on what you’re building from peers and the community, all in an informal setting. Started back in 2008, DemoCamp Edmonton has steadily grown into one of the largest in the country, with over 200 people attending each event.
-					(Hosted by Startup Edmonton)
 				</p>
+				<p>
+					RSVP for democamp <a href="http://www.startupedmonton.com/new-events/2017/1/24/democamp-34">here</a>.
+				</p>
+			</article>
 
-				<section>
-					<h3>Demos</h3>
-					<p style="margin:0px">To be determined - contact for more</p>
-					<div class="row" style="margin-top: 0px">
-						<div class="4u 12u$(mobile)">
-							<a href="#" class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/static/images/Demo01.jpg" alt=""></a>
-						</div>
-						<div class="4u 12u$(mobile)">
-							<a href="#" class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/static/images/Demo02.jpg" alt=""></a>
-						</div>
-						<div class="4u$ 12u$(mobile)">
-							<a href="#" class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/static/images/Demo03.jpg" alt=""></a>
-						</div>
-					</div>
-				</section>
-			</article>-->
+		<!-- Speaker -->
+			<article id="speaker" class="panel">
+				<header>
+					<h2>Distinguished Speaker: Hemi Thaker</h2>
+					4:30pm on Thursday January 26 @ <a href="https://goo.gl/maps/VE2qCo7nf6S2">8th floor DICE</a>
+				</header>
+				<h3>How We Built a Multi-million Dollar Tech Company</h3>
+				<br/>
+				<p>
+					Hemi is a University of Alberta alum, graduating from the Computer Engineering program in '86, and a very successful business entrepreneur. He earned his Masters in Electrical Engineering from the University of Waterloo and has launched two winning companies with very little start-up money. As the winner of the Ernst and Young Entrepreneur of the Year Award he hopes to inspire Canadians to take risks in their careers. He has had successes and what he calls successful failures over the years and with his vast life experience this proves to be a stimulating lecture for both Engineers and business enthusiasts.
+				</p>
+			</article>
 
 		<!-- Mixer -->
 			<article id="mixer" class="panel">
 				<header>
 					<h2>Industry Social</h2>
-				  When&Where
-              		(Friday at DICE 8th Floor)
+					5:30 PM on Friday, January 27 @ <a href="https://maps.google.com/maps?f=q&hl=en&q=ETLC">ETLC Solarium, University of Alberta</a>
 				</header>
 				<p>
-				  The Industry Social is an evening event comprised of tech talks and a mixer. Tech talks are 10 minute presentations about interesting technology, and its role and applications in industry. Come out and learn about the innovative technologies used by companies. After the talks, the mixer gives students, professors and industry representatives a chance to socialize, network and forge personal connections in a friendly setting. Food and beverages will be provided!
+					The Industry Social is an opportunity for electrical engineering, computer engineering, and engineering physics students to learn about the work and culture of engineering in industry and an opportunity for industry professionals to meet and guide students through the industry.
+					<br>The mixer is a casual setting for students and professions to connect and develop relationships - a good way for to create awareness of the career opportunities awesome technologies in our industry.
+					<br>The evening will have free snacks throughout the event on behalf of the Department of Electrical and Computer Engineering.
 				</p>
-				<!-- <span class="embed-youtube" style="text-align:center; display: block;"><iframe class="youtube-player" type="text/html" width="560" height="315" src="http://www.youtube.com/embed/videoseries?list=PLRp9jPf602CCu8xiaUwp-fZqAnMVAjIZ-&amp;hl=en_US" allowfullscreen="true" style="border:0;"></iframe></span> -->
-				<section>
-					<h3>Tech Talks</h3>
-					<p style="margin:0px">To be determined - contact for more</p>
-					<div class="row" style="margin-top: 0px">
-						<div class="4u 12u$(mobile)">
-							<a href="#" class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/static/images/TechTalk01.jpg" alt=""></a>
-						</div>
-						<div class="4u 12u$(mobile)">
-							<a href="#" class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/static/images/TechTalk02.jpg" alt=""></a>
-						</div>
-						<div class="4u$ 12u$(mobile)">
-							<a href="#" class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/static/images/TechTalk03.jpg" alt=""></a>
-						</div>
-						<div class="4u 12u$(mobile)">
-							<a href="#" class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/static/images/TechTalk04.jpg" alt=""></a>
-						</div>
-						<div class="4u 12u$(mobile)">
-							<a href="#" class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/static/images/TechTalk05.jpg" alt=""></a>
-						</div>
-						<div class="4u$ 12u$(mobile)">
-							<a href="#" class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/static/images/TechTalk06.jpg" alt=""></a>
-						</div>
-					</div>
-				</section>
+				<p>
+					RSVP <a href="http://eceweekmixer.eventbrite.ca">here</a>
+				</p>
 			</article>
 
 		<!-- Hack -->
-			<article id="hack" class="panel">
-				<header>
-					<h2>HackEd 2017</h2>
-					Saturday and Sunday, January 28-29th. At <a color="inherit" href="https://www.google.ca/maps/place/Startup+Edmonton">StartUP Edmonton.</a>
-				</header>
-				<p>
-					HackEd is our student hackathon, an event where students have 24 hours to design, implement and present innovative solutions to life’s problems. Using software, hardware and collaboration, students get a chance to exercise their creativity and problem solving skills to build something awesome! Students of all disciplines and skill levels are welcome to join. Learn more about HackEd 2017 on the  <a color="inherit" href="https://hacked.compeclub.com">HackEd website</a>. Previous hacks can be found on the <a color="inherit" href="http://hacked2016.devpost.com/">HackEd2016 Devpost</a> page.
-				</p>
-				<section>
-					<div class="row">
-						<div class="4u 12u$(mobile)">
-							<a href="#" class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/images/HackED.jpg" alt=""></a>
-						</div>
-						<div class="4u 12u$(mobile)">
-							<a href="#" class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/images/HackED2.JPG" alt=""></a>
-						</div>
-						<div class="4u$ 12u$(mobile)">
-							<a href="#" class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/images/HackED3.jpg" alt=""></a>
-						</div>
-						<div class="4u 12u$(mobile)">
-							<a href="#" class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/images/HackED4.jpg" alt=""></a>
-						</div>
-						<div class="4u 12u$(mobile)">
-							<a href="#" class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/images/HackED7.JPG" alt=""></a>
-						</div>
-						<div class="4u$ 12u$(mobile)">
-							<a href="#" class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/images/HackED9.JPG" alt=""></a>
-						</div>
-					</div>
-				</section>
-			</article>
+							<article id="hack" class="panel">
+								<header>
+									<h2>HackEd 2017</h2>
+									Saturday and Sunday, January 28-29th @ <a color="inherit" href="https://www.google.ca/maps/place/Startup+Edmonton">StartUP Edmonton.</a>
+								</header>
+								<p>
+									HackEd is our student hackathon, an event where students have 24 hours to design, implement and present innovative solutions to life’s problems. Using software, hardware and collaboration, students get a chance to exercise their creativity and problem solving skills to build something awesome! Students of all disciplines and skill levels are welcome to join. <br/>
+									<button color="inherit"><a href="https://hacked.compeclub.com">Apply & Learn More</a></button>
+									<button color="inherit"><a href="http://hacked2016.devpost.com/">HackEd2016 Hacks</a></button>
+								</p>
+								<section>
+									<div class="row">
+										<div class="4u 12u$(mobile)">
+											<div class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/images/hacked/HackED.jpg" alt=""></div>
+										</div>
+										<div class="4u 12u$(mobile)">
+											<div class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/images/hacked/HackED2.JPG" alt=""></div>
+										</div>
+										<div class="4u$ 12u$(mobile)">
+											<div class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/images/hacked/HackED3.jpg" alt=""></div>
+										</div>
+										<div class="4u 12u$(mobile)">
+											<div class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/images/hacked/HackED4.jpg" alt=""></div>
+										</div>
+										<div class="4u 12u$(mobile)">
+											<div class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/images/hacked/HackED7.JPG" alt=""></div>
+										</div>
+										<div class="4u$ 12u$(mobile)">
+											<div class="image fit"><img src="https://cdn.rawgit.com/UAlbertaCompEClub/eceweekwebsite2017/master/images/hacked/HackED9.JPG" alt=""></div>
+										</div>
+									</div>
+								</section>
+							</article>
 
 		<!-- Contact -->
 			<article id="contact" class="panel">
